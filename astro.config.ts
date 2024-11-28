9b352c9eb1ef6a4ec0b59d6860a3b5f39fdb93f0
Dima Mishin <dimm@protonmail.ch>
2024-11-28 06:00:00 +0000

    GitLab pages deploy

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,11 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://gitlab.nrp-nautilus.io',
+  base: '/prp/nrp-site',
   output: 'static',
+  outDir: 'public',
+  publicDir: 'static',
 
   integrations: [
     starlight({
