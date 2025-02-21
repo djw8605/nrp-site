74982c1c93c0f77086e8e1339a1417d1dd66173b
Mohammad Sada <mfsada@mohammads-air.mynetworksettings.com>
2025-02-20 20:38:58 -0500

    fixed sidebar

--- astro.config.ts
+++ astro.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   output: 'static',
   outDir: 'public',
   publicDir: 'static',
-  
+
   integrations: [
     starlight({
       favicon: '/static/favicon.ico',
@@ -39,10 +39,6 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: "Need help?",
-          items: [{ label: "Index", link: "documentation" }],
-        },
-        {
           label: "User Guide",
           items: [
             {
@@ -51,6 +47,7 @@ export default defineConfig({
             },
             {
               label: "Start",
+              collapsed: true,
               items: [
                 { label: "Get Access", link: "documentation/userdocs/start/get-access" },
                 { label: "Quick Start", link: "documentation/userdocs/start/quickstart" },
@@ -65,6 +62,7 @@ export default defineConfig({
             },
             {
               label: "Tutorials",
+              collapsed: true,
               items: [
                 { label: "Introduction", link: "documentation/userdocs/tutorial/introduction" },
                 { label: "Docker and Containers", link: "documentation/userdocs/tutorial/docker" },
@@ -82,7 +80,68 @@ export default defineConfig({
               ],
             },
             {
+              "label": "Running",
+              collapsed: true,
+              "items": [
+                {
+                  "label": "Beginner start",
+                  collapsed: true,
+                  "items": [
+                    { "label": "GPU pods", "link": "documentation/userdocs/running/gpu-pods" },
+                    { "label": "Long idle pods", "link": "documentation/userdocs/running/long-idle" },
+                    { "label": "Monitoring", "link": "documentation/userdocs/running/monitoring" },
+                    { "label": "Running batch jobs", "link": "documentation/userdocs/running/jobs" },
+                    { "label": "Running CPU only jobs", "link": "documentation/userdocs/running/cpu-only" },
+                    { "label": "Scheduling", "link": "documentation/userdocs/running/scheduling" }
+                  ]
+                },
+                {
+                  "label": "Intermediate",
+                  collapsed: true,
+                  "items": [
+                    { "label": "Client scripts", "link": "documentation/userdocs/running/scripts" },
+                    { "label": "Exposing HTTP", "link": "documentation/userdocs/running/ingress" },
+                    { "label": "Special use", "link": "documentation/userdocs/running/special" },
+                    { "label": "Faster images download", "link": "documentation/userdocs/running/fast-img-download" },
+                    { "label": "Globus-connect", "link": "documentation/userdocs/running/globus-connect" },
+                    { "label": "Python k8s API", "link": "documentation/userdocs/running/kubernetes" },
+                    { "label": "Federation", "link": "documentation/userdocs/running/federation" },
+                    { "label": "GUI Desktop", "link": "documentation/userdocs/running/gui-desktop" },
+                    { "label": "Scientific images", "link": "documentation/userdocs/running/sci-img" },
+                    { "label": "Postgres cluster", "link": "documentation/userdocs/running/postgres" }
+                  ]
+                },
+                {
+                  "label": "Virtualization",
+                  collapsed: true,
+                  "items": [
+                    { "label": "General", "link": "documentation/userdocs/running/virtualization-general" },
+                    { "label": "Live ubuntu", "link": "documentation/userdocs/running/virtualization-ubuntu" },
+                    { "label": "Windows", "link": "documentation/userdocs/running/virtualization-windows" }
+                  ]
+                },
+                {
+                  "label": "Distributed computing",
+                  collapsed: true,
+                  "items": [
+                    { "label": "Ray Cluster", "link": "documentation/userdocs/running/ray-cluster" },
+                    { "label": "Dask Cluster", "link": "documentation/userdocs/running/dask-cluster" },
+                    { "label": "Kubeflow", "link": "documentation/userdocs/running/kubeflow" }
+                  ]
+                },
+                {
+                  "label": "Performance",
+                  collapsed: true,
+                  "items": [
+                    { "label": "High I/O jobs", "link": "documentation/userdocs/running/io-jobs" },
+                    { "label": "CPU throttling", "link": "documentation/userdocs/running/cpu-throttling" }
+                  ]
+                }
+              ]
+            },
+            {
               label: "Jupyter",
+              collapsed: true,
               items: [
                 { label: "JupyterHub Service", link: "documentation/userdocs/jupyter/jupyterhub-service" },
                 { label: "ML/Jupyter Pod", link: "documentation/userdocs/jupyter/jupyter-pod" },
@@ -91,116 +150,213 @@ export default defineConfig({
             },
             {
               label: "Coder",
+              collapsed: true,
               items: [
                 { label: "Using Coder", link: "documentation/userdocs/coder/coder" },
                 { label: "Deploying Coder", link: "documentation/userdocs/coder/deploy" },
               ],
             },
             {
-              label: "AI",
+              label: "AI/LLMs",
+              collapsed: true,
               items: [
                 { label: "NRP Managed LLM", link: "documentation/userdocs/ai/llm-managed" },
                 { label: "LLM as a Service", link: "documentation/userdocs/ai/llm-service" },
                 { label: "LLM in JupyterHub", link: "documentation/userdocs/ai/llm-jupyterhub" },
               ],
             },
+            {
+              label: "FPGA",
+              collapsed: true,
+              items: [
+                { label: "Vivado and Vitis", link: "documentation/userdocs/fpgas/vivado-vitis" },
+                { label: "ESnet SmartNIC", link: "documentation/userdocs/fpgas/esnet" },
+              ],
+            },
+            {
+              "label": "Storage",
+              collapsed: true,
+              "items": [
+                {
+                  "label": "Storage options",
+                  collapsed: true,
+                  "items": [
+                    { "label": "Intro", "link": "documentation/userdocs/storage/intro" },
+                    { "label": "Ceph FS / RBD", "link": "documentation/userdocs/storage/ceph" },
+                    { "label": "Ceph S3", "link": "documentation/userdocs/storage/ceph-s3" },
+                    { "label": "CVMFS", "link": "documentation/userdocs/storage/cvmfs" },
+                    { "label": "Local scratch", "link": "documentation/userdocs/storage/local" },
+                    { "label": "Linstor", "link": "documentation/userdocs/storage/linstor" },
+                    { "label": "Nextcloud", "link": "documentation/userdocs/storage/nextcloud" },
+                    { "label": "Syncthing", "link": "documentation/userdocs/storage/syncthing" }
+                  ]
+                },
+                {
+                  "label": "Managing",
+                  collapsed: true,
+                  "items": [
+                    { "label": "Moving data", "link": "documentation/userdocs/storage/move-data" },
+                    { "label": "Purging", "link": "documentation/userdocs/storage/purging" }
+                  ]
+                }
+              ]
+            },
+            {
+              label: "Development",
+              collapsed: true,
+              items: [
+                { label: "Building in GitLab", link: "documentation/userdocs/development/gitlab" },
+                { label: "Private Repos", link: "documentation/userdocs/development/private-repos" },
+                { label: "Integrating GitLab and k8s", link: "documentation/userdocs/development/k8s-integration" },
+              ],
+            },
           ],
         },
         {
-          label: "Admin Guide",
-          items: [
+          "label": "Admin guide",
+          collapsed: true,
+          "items": [
             {
-              label: "Participating",
-              items: [
-                { label: "Networking", link: "documentation/admindocs/participating/network" },
-                { label: "Joining a Server", link: "documentation/admindocs/participating/new-contributor-guide" },
-              ],
+              "label": "Participating",
+              collapsed: true,
+              "items": [
+                { "label": "Networking", "link": "documentation/admindocs/participating/network" },
+                { "label": "Joining a server", "link": "documentation/admindocs/participating/new-contributor-guide" }
+              ]
             },
             {
-              label: "Perfsonar",
-              items: [
-                { label: "Install", link: "documentation/admindocs/perfsonar/install" },
-                { label: "Workshop Links", link: "documentation/admindocs/perfsonar/maddash" },
-                { label: "A2A Maddash", link: "documentation/admindocs/perfsonar/a2a" },
-              ],
+              "label": "Perfsonar",
+              collapsed: true,
+              "items": [
+                { "label": "Install", "link": "documentation/admindocs/perfsonar/install" },
+                { "label": "Workshop links", "link": "documentation/admindocs/perfsonar/maddash" },
+                { "label": "A2A Maddash", "link": "documentation/admindocs/perfsonar/a2a" }
+              ]
             },
             {
-              label: "Cluster Admin",
-              items: [
-                { label: "Cluster User Management", link: "documentation/admindocs/cluster/cluster-user-mgmt" },
-                { label: "Cluster Node Management", link: "documentation/admindocs/cluster/node-mgmt" },
-                { label: "JupyterLab", link: "documentation/admindocs/cluster/jupyterlab-admin" },
-                { label: "KubeVirt", link: "documentation/admindocs/cluster/kubevirt-gpu" },
-                { label: "Services Topology", link: "documentation/admindocs/cluster/cdn-svc" },
-                { label: "KubeRay Operator", link: "documentation/admindocs/cluster/ray-operator" },
-                { label: "SENSE/Multus", link: "documentation/admindocs/cluster/sense-multus" },
-                { label: "Partitioning MIG GPUs", link: "documentation/admindocs/cluster/mig-gpus" },
-              ],
+              "label": "FIONA",
+              collapsed: true,
+              "items": [
+                { "label": "Builds", "link": "documentation/admindocs/fiona/builds" },
+                { "label": "Install", "link": "documentation/admindocs/fiona/install" }
+              ]
             },
             {
-              label: "Upgrades",
-              items: [
-                { label: "Intro", link: "documentation/admindocs/upgrades/intro" },
-                { label: "Calico", link: "documentation/admindocs/upgrades/calico" },
-                { label: "Cert-Manager", link: "documentation/admindocs/upgrades/cert-manager" },
-                { label: "Coder", link: "documentation/admindocs/upgrades/coder" },
-                { label: "ElasticSearch", link: "documentation/admindocs/upgrades/elasticsearch" },
-                { label: "GitLab", link: "documentation/admindocs/upgrades/gitlab" },
-                { label: "JupyterHub", link: "documentation/admindocs/upgrades/jupyterhub" },
-                { label: "K8s", link: "documentation/admindocs/upgrades/k8s" },
-                { label: "KubeVirt", link: "documentation/admindocs/upgrades/kubevirt" },
-                { label: "Nextcloud", link: "documentation/admindocs/upgrades/nextcloud" },
-                { label: "Nodes", link: "documentation/admindocs/upgrades/nodes" },
-                { label: "Prometheus", link: "documentation/admindocs/upgrades/prometheus" },
+              "label": "NRP",
+              collapsed: true,
+              "items": [
+                { "label": "Topology", "link": "documentation/admindocs/nrp/topology" }
+              ]
+            },
+            {
+              "label": "Storage",
+              collapsed: true,
+              "items": [
+                { "label": "Broken drives", "link": "documentation/admindocs/storage/broken-drives" },
+                { "label": "Ceph S3", "link": "documentation/admindocs/storage/ceph-s3" },
+                { "label": "Ceph Recover", "link": "documentation/admindocs/storage/ceph" },
+                { "label": "Ceph Performance", "link": "documentation/admindocs/storage/ceph-performance" },
+                { "label": "Volume Mounting", "link": "documentation/admindocs/storage/volume-mounting" },
+                { "label": "Origin SyncThing", "link": "documentation/admindocs/storage/origin-sync" }
+              ]
+            },
+            {
+              "label": "Vault",
+              collapsed: true,
+              "items": [
+                { "label": "Getting certs", "link": "documentation/admindocs/vault/cert" }
+              ]
+            },
+            {
+              "label": "Links",
+              collapsed: true,
+              "items": [
+                { "label": "Hardware", "link": "documentation/admindocs/links/hardware" },
+                { "label": "Knowledge base", "link": "documentation/admindocs/links/knowledge-base" },
+                { "label": "Software", "link": "documentation/admindocs/links/software" },
+                { "label": "Vis", "link": "documentation/admindocs/links/vis" }
+              ]
+            },
+            {
+              "label": "Cluster admin",
+              collapsed: true,
+              "items": [
+                { "label": "Cluster user management", "link": "documentation/admindocs/cluster/cluster-user-mgmt" },
+                { "label": "Cluster node management", "link": "documentation/admindocs/cluster/node-mgmt" },
+                { "label": "JupyterLab", "link": "documentation/admindocs/cluster/jupyterlab-admin" },
+                { "label": "KubeVirt", "link": "documentation/admindocs/cluster/kubevirt-gpu" },
+                { "label": "Services topology", "link": "documentation/admindocs/cluster/cdn-svc" },
+                { "label": "KubeRay operator", "link": "documentation/admindocs/cluster/ray-operator" },
+                { "label": "SENSE/Multus", "link": "documentation/admindocs/cluster/sense-multus" },
+                { "label": "Partitioning MIG GPUs", "link": "documentation/admindocs/cluster/mig-gpus" },
                 {
-                  label: "Storage",
-                  items: [
-                    { label: "Rook/Ceph", link: "documentation/admindocs/upgrades/rook" },
-                    { label: "Linstor", link: "documentation/admindocs/upgrades/piraeus-operator" },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
+                  "label": "Upgrades",
+                  collapsed: true,
+                  "items": [
+                    { "label": "intro", "link": "documentation/admindocs/upgrades/intro" },
+                    { "label": "calico", "link": "documentation/admindocs/upgrades/calico" },
+                    { "label": "cert-manager", "link": "documentation/admindocs/upgrades/cert-manager" },
+                    { "label": "coder", "link": "documentation/admindocs/upgrades/coder" },
+                    { "label": "elasticsearch", "link": "documentation/admindocs/upgrades/elasticsearch" },
+                    { "label": "gitlab", "link": "documentation/admindocs/upgrades/gitlab" },
+                    { "label": "jupyterhub", "link": "documentation/admindocs/upgrades/jupyterhub" },
+                    { "label": "k8s", "link": "documentation/admindocs/upgrades/k8s" },
+                    { "label": "kubevirt", "link": "documentation/admindocs/upgrades/kubevirt" },
+                    { "label": "nextcloud", "link": "documentation/admindocs/upgrades/nextcloud" },
+                    { "label": "nodes", "link": "documentation/admindocs/upgrades/nodes" },
+                    { "label": "prometheus", "link": "documentation/admindocs/upgrades/prometheus" },
+                    {
+                      "label": "Storage",
+                      collapsed: true,
+                      "items": [
+                        { "label": "rook/ceph", "link": "documentation/admindocs/upgrades/rook" },
+                        { "label": "linstor", "link": "documentation/admindocs/upgrades/piraeus-operator" }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
       ],
     }),
     tailwind({
-    applyBaseStyles: true,
+      applyBaseStyles: true,
     }), sitemap(), mdx(), icon({
-    include: {
-      tabler: ['*'],
-      mdi: ["*"],
-      'flat-color-icons': [
-        'template',
-        'gallery',
-        'approval',
-        'document',
-        'advertising',
-        'currency-exchange',
-        'voice-presentation',
-        'business-contact',
-        'database',
-      ],
-    },
-  }), ...whenExternalScripts(() =>
-    partytown({
-      config: { forward: ['dataLayer.push'] },
-    })
-  ), compress({
-    CSS: true,
-    HTML: {
-      'html-minifier-terser': {
-        removeAttributeQuotes: false,
+      include: {
+        tabler: ['*'],
+        mdi: ["*"],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
       },
-    },
-    Image: false,
-    JavaScript: true,
-    SVG: false,
-    Logger: 1,
-  }), astrowind({
-    config: './src/config.yaml',
-  })],
+    }), ...whenExternalScripts(() =>
+      partytown({
+        config: { forward: ['dataLayer.push'] },
+      })
+    ), compress({
+      CSS: true,
+      HTML: {
+        'html-minifier-terser': {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: false,
+      Logger: 1,
+    }), astrowind({
+      config: './src/config.yaml',
+    })],
 
   image: {
     domains: ['cdn.pixabay.com', 'media.nrp.ai', 'portal.nrp-nautilus.io'],
