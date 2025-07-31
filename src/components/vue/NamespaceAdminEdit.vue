e0b1c5cd84c207183c5770c12be7bf25946fc372
Mohammad Firas Sada <mfsada@ucsd.edu>
2025-07-31 13:33:23 -0400

    Try fix dropdown

--- src/components/vue/NamespaceAdminEdit.vue
+++ src/components/vue/NamespaceAdminEdit.vue
@@ -19,7 +19,7 @@
                     </FloatLabel>
                     <Message v-if="$form.description?.invalid" severity="error" size="small" variant="simple">{{ $form.description.error?.message }}</Message>
                     <FloatLabel variant="on">
-                        <AutoComplete name="institution" forceSelection id="institution" type="text" :suggestions="filteredOrganizations" @complete="getOrganizationsFilter" fluid />
+                        <AutoComplete name="institution" forceSelection optionLabel="name" id="institution" type="text" :suggestions="filteredOrganizations" @complete="getOrganizationsFilter" fluid />
                         <label for="institution">Institution</label>
                     </FloatLabel>
                     <Message v-if="$form.institution?.invalid" severity="error" size="small" variant="simple">{{ $form.institution.error?.message }}</Message>
@@ -317,7 +317,7 @@ const getOrganizationsFilter = (org) => {
                 return response.json();
             })
             .then(data => {
-                filteredOrganizations.value = data.items.map(item => item.name);
+                filteredOrganizations.value = data.items.map(item => ({ name: item.name }));
                 
                 // resolve(organizations);
                 resolve();
