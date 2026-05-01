604c5220a9bda34f1bac657f0098254178efb584
Derek Weitzel <djw8605@gmail.com>
2026-05-01 09:12:35 -0500

    renaming the page running jobs diagnose to just diagnose

--- src/navigation.ts
+++ src/navigation.ts
@@ -131,7 +131,7 @@ export const headerData = {
         },
         {
           text: 'Pod Diagnosis',
-          href: getPermalink('/runningpods'),
+          href: getPermalink('/diagnose'),
         },
         {
           text: 'Resources',
