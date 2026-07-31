2f5cd7b3222ca71ddeb302cd47ea1033cc018776
Sam <55770131+Sam6734@users.noreply.github.com>
2026-07-31 17:15:19 -0500

    interlink sidebar

--- astro.config.ts
+++ astro.config.ts
@@ -285,6 +285,15 @@ export default defineConfig({
               items: [
                 { label: 'Networking', link: 'documentation/admindocs/participating/network' },
                 { label: 'Joining a server', link: 'documentation/admindocs/participating/new-contributor-guide' },
+                { label: 'Connect Slurm with InterLink', link: 'documentation/admindocs/participating/interlink' },
+                {
+                  label: 'Install an InterLink Virtual Node',
+                  link: 'documentation/admindocs/participating/interlink-kubernetes',
+                },
+                {
+                  label: 'Ceph S3 for InterLink Jobs',
+                  link: 'documentation/admindocs/participating/interlink-storage',
+                },
               ],
             },
             {
